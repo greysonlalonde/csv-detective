887e5a9a618b8d2e59b9ca8ed0cc77cb1f51bddc
greysonlalonde <greyson.r.lalonde@gmail.com>
2021-02-19 14:42:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
 setup(
-   name='searchValues',
+   name='csv-detective',
+   packages = ['csv-detective']
    version='0.1.0',
-   author='G LaLonde',
-   author_email='lalondeg1@michigan.gov',
-   description='Search a folder of csv files!',
+   author='Greyson R. LaLonde',
+   author_email='greyson.r.lalonde@gmail.com',
+   description='Search a folder of csv files for value(s)',
+   url = 'https://github.com/greysonlalonde/csv-detective',
+    download_url = 'https://github.com/greysonlalonde/csv-detective/archive/0.0.1.tar.gz',
+   keywords = ['tag1', 'tag2'],
    install_requires=[
-       "pandas >= 1.2.1",
+    "pandas >= 1.2.1",
    ],
+   classifiers = []
 )
